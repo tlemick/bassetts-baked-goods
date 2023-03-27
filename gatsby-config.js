21a5f9fd0f72e4a4f5e3956a89cfba6e14db6d8d
tlemick <tlemick@gmail.com>
2023-03-27 16:29:50 -0700

    removed gtag plugin to fix 404 build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-styled-components",
-        "gatsby-plugin-google-gtag",
         "gatsby-plugin-catch-links",
         "gatsby-plugin-mdx",
         {
